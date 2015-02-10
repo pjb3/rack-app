18c569a2b007e19afdb2629925f58fc794b71b73
Paul Barry <mail@paulbarry.com>
2015-02-09 19:21:06 -0500

    Initialize the session message to be an empty array

--- lib/messages_controller.rb
+++ lib/messages_controller.rb
@@ -1,13 +1,12 @@
 class MessagesController < ApplicationController
   def show
-    @messages = Array(session[:messages])
+    @messages = session[:messages] ||= []
     render "messages/show"
   end
 
   def create
-    @messages = Array(session[:messages])
+    @messages = session[:messages] ||= []
     @messages << params.merge("timestamp" => Time.now)
-    session[:messages] = @messages
     redirect_to "/messages"
   end
 end
